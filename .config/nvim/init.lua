bab5e8cd45375653e0e20e306024b63878d6f32f
bleh <107344734+CuterThanYou@users.noreply.github.com>
2024-06-29 00:00:00 +0000

    neovim initial commit

---
+++ .config/nvim/init.lua
@@ -0,0 +1,8 @@
+vim.wo.number = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#89b4fa' })
+vim.api.nvim_set_hl(0, 'LineNr', { fg='#f5c2e7' })
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#89b4fa' })
