b12d5549459f4b34717bb473a70cf08e804f6c03
bleh <107344734+CuterThanYou@users.noreply.github.com>
2024-07-06 18:51:45 +0630

    added shortcut for system clipboard

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,6 +3,10 @@ vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 
+local map = vim.keymap.set
+map('v', '<C-c>', '"+y')
+map('n', '<C-v>', '"+p')
+
 vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#89b4fa' })
 vim.api.nvim_set_hl(0, 'LineNr', { fg='#f5c2e7' })
 vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#89b4fa' })
