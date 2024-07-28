433d8d446ce996ccca0821539a3a5b706467744b
bleh <107344734+CuterThanYou@users.noreply.github.com>
2024-07-28 18:35:38 +0630

    restore cursor on exit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,3 +10,11 @@ map('n', '<C-v>', '"+p')
 vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#89b4fa' })
 vim.api.nvim_set_hl(0, 'LineNr', { fg='#f5c2e7' })
 vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#89b4fa' })
+
+-- restore cursor on exit
+vim.cmd [[
+:augroup RestoreCursorShapeOnExit
+:    autocmd!
+:    autocmd VimLeave * set guicursor=a:hor20
+:augroup END
+]]
