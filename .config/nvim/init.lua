8c42a9454ed4f36641179c4b5714949be8aeb111
bleh <107344734+CuterThanYou@users.noreply.github.com>
2024-09-27 00:00:00 +0000

    use terminal background instead of colortheme background

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,14 +3,17 @@ vim.opt.relativenumber = true
 vim.opt.shiftwidth = 3
 vim.opt.tabstop = 3
 
+-- keybinding
 local map = vim.keymap.set
 map('v', '<C-c>', '"+y')
 map('n', '<C-v>', '"+p')
 map('v', '<Space>', '<C-w>')
 map('n', '<Space>', '<C-w>')
 
+-- color
 vim.cmd.colorscheme('catppuccin_mocha')
 
+vim.api.nvim_set_hl(0, 'Normal', {ctermbg = 'NONE'}) -- use terminal background
 vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#89b4fa' })
 vim.api.nvim_set_hl(0, 'LineNr', { fg='#f5c2e7' })
 vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#89b4fa' })
