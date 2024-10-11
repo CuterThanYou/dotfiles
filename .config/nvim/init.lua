815ff712d7959e1cdcd6c86baf695dfd785e19de
bleh <107344734+CuterThanYou@users.noreply.github.com>
2024-10-11 00:00:00 +0000

    removed space macro && commented better

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,21 +1,27 @@
-vim.wo.number = true
-vim.opt.relativenumber = true
-vim.opt.shiftwidth = 3
-vim.opt.tabstop = 3
-vim.opt.mouse="" -- there is no palm rejection :/
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
 -- keybinding
 local map = vim.keymap.set
+-- system clipboard
 map('v', '<C-c>', '"+y')
 map('n', '<C-v>', '"+p')
 map('i', '<C-v>', '<Esc>"+pa')
-map('v', '<Space>', '<C-w>')
-map('n', '<Space>', '<C-w>')
+
+-- line number
+vim.wo.number = true
+vim.opt.relativenumber = true
+-- tab spaces
+vim.opt.shiftwidth = 3
+vim.opt.tabstop = 3
+-- disable mouse
+vim.opt.mouse=""
+-- case sensitive search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
 -- color
 vim.cmd.colorscheme('catppuccin_mocha')
-vim.api.nvim_set_hl(0, 'Normal', {ctermbg = 'NONE'}) -- use terminal background
+-- use terminal background
+vim.api.nvim_set_hl(0, 'Normal', {ctermbg = 'NONE'})
+-- color for line number
 vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#cba6f7' })
 vim.api.nvim_set_hl(0, 'LineNr', { fg='#74c7ec' })
 vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#cba6f7' })
