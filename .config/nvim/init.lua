48e31b40cd9cec79b34100da2f253a97b23980b3
bleh <107344734+CuterThanYou@users.noreply.github.com>
2024-10-15 00:00:00 +0000

    add leader key binds && trying out plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,30 @@
 -- keybinding
+vim.g.mapleader = " "
 local map = vim.keymap.set
 -- system clipboard
 map('v', '<C-c>', '"+y')
 map('n', '<C-v>', '"+p')
 map('i', '<C-v>', '<Esc>"+pa')
 
+map('n', '<Leader>r', ':so ~/.config/nvim/init.lua<CR>') -- reload config
+
+-- tab management
+map('n', '<Leader>t', ':tabnew<CR>')
+map('n', 'g1', '1gt')
+map('n', 'g2', '2gt')
+map('n', 'g3', '3gt')
+map('n', 'g4', '4gt')
+map('n', 'g5', '5gt')
+map('n', 'g6', '6gt')
+map('n', 'g7', '7gt')
+map('n', 'g8', '8gt')
+map('n', 'g9', '9gt')
+
+-- plugins
+require('nvim-highlight-colors').setup({})
+
+-- netrw
+vim.g.netrw_banner = 0
 -- line number
 vim.wo.number = true
 vim.opt.relativenumber = true
@@ -18,6 +38,7 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 -- color
+vim.opt.termguicolors = true
 vim.cmd.colorscheme('catppuccin_mocha')
 -- use terminal background
 vim.api.nvim_set_hl(0, 'Normal', {ctermbg = 'NONE'})
