d33dfcaab676c079ab37ec4d1020b7ccbc264e2a
bleh <107344734+CuterThanYou@users.noreply.github.com>
2024-10-22 00:00:00 +0000

    configuring status bar, hate those stupid block

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -4,9 +4,24 @@ Header:children_add(function()
 		return ui.Line {}
 	end
 	return ui.Line {
-		ui.Span(ya.user_name()):fg("blue"),
+		ui.Span(ya.user_name()):fg("magenta"),
 		ui.Span("@"),
 		ui.Span(ya.host_name()):fg("magenta"),
 		ui.Span(":"),
 	}
 end, 500, Header.LEFT)
+
+-- show user and group
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if h == nil or ya.target_family() ~= "unix" then
+		return ui.Line {}
+	end
+
+	return ui.Line {
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		ui.Span(":"),
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		ui.Span(" "),
+	}
+end, 500, Status.RIGHT)
