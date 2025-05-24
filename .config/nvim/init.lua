ddc07731d0e78309ab3db1ccf99291463e2b6ac8
bleh <107344734+CuterThanYou@users.noreply.github.com>
2025-05-24 00:00:00 +0000

    nvim changes
    remove lsp
    disable shada
    use terminal background on dark theme
    enable setting cursor on quit
    and some organizing

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -24,27 +24,13 @@ map('n', 'g9', '9gt')
 
 -- plugins {{{
 require('nvim-highlight-colors').setup({})
-
--- language server
-local lsp = require('lspconfig')
--- rust {{{
-lsp.rust_analyzer.setup {
-	settings = {
-		['rust-analyzer'] = {
-			check = {
-				command = "clippy";
-			},
-		}
-	},
-
-	-- don't recolor
-	on_attach = function(client)
-		client.server_capabilities.semanticTokensProvider = nil
-	end,
-} -- }}}
 -- }}}
 
+-- features {{{
 vim.opt.scroll = math.floor(0.3 * vim.o.lines) -- 1/3 scroll for ctrl-d, ctrl-u
+-- these are more annoying than useful
+vim.g.leave_my_cursor_position_alone = 1
+vim.opt.shadafile = "NONE"
 vim.opt.swapfile = false -- no auto-save
 -- netrw
 vim.g.netrw_banner = 0
@@ -55,8 +41,6 @@ vim.opt.relativenumber = true
 -- tab spaces
 vim.opt.shiftwidth = 3
 vim.opt.tabstop = 3
--- disable mouse
-vim.opt.mouse=""
 -- case sensitive search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
@@ -75,15 +59,6 @@ vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 
--- color
-vim.opt.termguicolors = true
-vim.cmd.colorscheme('catppuccin')
--- use terminal background
-vim.api.nvim_set_hl(0, 'Normal', {ctermbg = 'NONE'})
--- color for line number
-vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#cba6f7' })
-vim.api.nvim_set_hl(0, 'LineNr', { fg='#74c7ec' })
-vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#cba6f7' })
 -- enable tree-shitter
 vim.api.nvim_create_autocmd('FileType', {
 	pattern = {
@@ -93,12 +68,33 @@ vim.api.nvim_create_autocmd('FileType', {
 		vim.treesitter.start()
 	end
 })
+-- }}}
+
+-- color {{{
+vim.opt.termguicolors = true
+
+-- use terminal background
+vim.api.nvim_create_autocmd({ 'ColorScheme' }, { -- {{{
+ 	pattern = {'*'},
+ 	callback = function()
+		if vim.api.nvim_get_option_value('background',{}) == 'dark' then
+			vim.api.nvim_set_hl(0, 'Normal', { ctermbg = 'NONE' })
+		end
+ 	end
+}) -- }}}
+
+vim.cmd.colorscheme('catppuccin')
+-- color for line number
+vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#cba6f7' })
+vim.api.nvim_set_hl(0, 'LineNr', { fg='#74c7ec' })
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#cba6f7' })
+-- }}}
 
 -- languages specifics {{{
 vim.g.c_syntax_for_h = true
--- fuck you rust doc, can't tell me what to do, 4 spaces my ass
-vim.g.rust_recommended_style = false -- use tab instead of space
-vim.g.python_recommended_style = false -- shite language
+-- ignore their dumb formatting guide
+vim.g.rust_recommended_style = false
+vim.g.python_recommended_style = false
 -- use space instaed of tab for haskell
 vim.api.nvim_create_autocmd('FileType', {
 	pattern = { 'haskell' },
@@ -123,14 +119,13 @@ vim.api.nvim_create_autocmd('FileType', {
 	end
 }) -- }}}
 
-
--- restore cursor on exit (not needed if u use vi mode in your interactive shell) {{{
---[[
+-- restore cursor on exit {{{
+-- https://github.com/neovim/neovim/issues/4396
 vim.api.nvim_create_autocmd({ 'VimLeave', 'VimSuspend' }, {
  	pattern = {'*'},
  	callback = function()
- 		vim.o.guicursor = "a:hor20"
+ 		vim.o.guicursor = "a:hor20-blinkon500-blinkoff500"
  	end
 }) -- }}}
-]]--
+
 -- vim:foldmethod=marker
